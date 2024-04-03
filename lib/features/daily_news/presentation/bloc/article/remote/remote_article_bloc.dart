8ab4c5b1b6dce811728367649c35fcb37a7034c2
unknown <abusayemsarker8@gmail.com>
2024-04-04 05:38:43 +0600

    implementation of bloc

---
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:news_app_clean_architecture/core/resources/data_state.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/get_article_use_case.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class RemoteArticlesBloc extends Bloc<RemoteArticleEvent, RemoteArticleState> {
+  final GetArticleUseCase _articleUseCase;
+
+  RemoteArticlesBloc(this._articleUseCase)
+      : super(const RemoteArticlesLoading()) {
+    on<GetArticles>(onGetArticles);
+  }
+
+  void onGetArticles(
+      GetArticles event, Emitter<RemoteArticleState> emit) async {
+    final dataState = await _articleUseCase();
+    if (dataState is DataSuccess && dataState.data!.isNotEmpty) {
+      emit(RemoteArticlesDone(dataState.data!));
+    }
+    if (dataState is DataFailed) {
+      emit(RemoteArticlesError(dataState.error!));
+    }
+  }
+}
